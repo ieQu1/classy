43471cfb4edeeaee6494b323d27fc7d7a9bbe2af
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2026-03-31 15:06:39 +0200

    Add autocluster strategies (WIP)

---
+++ src/discovery/classy_discovery_dns.erl
@@ -0,0 +1,57 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2019-2022, 2026 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+-module(classy_discovery_dns).
+
+-behaviour(classy_discovery_strategy).
+
+%% Cluster strategy callbacks
+-export([ discover/1
+        , lock/1
+        , unlock/1
+        , register/1
+        , unregister/1
+        ]).
+
+discover(Options) ->
+    DomainName = proplists:get_value(name, Options),
+    NodeName = proplists:get_value(app, Options, undefined),
+    Type = proplists:get_value(type, Options, a),
+    {ok, [node_name(NodeName, Host) || Host <- resolve_hosts(DomainName, Type)]}.
+
+resolve_hosts(DomainName, Type) when Type =:= a; Type =:= aaaa ->
+    [inet:ntoa(IP) || IP <- inet_res:lookup(DomainName, in, Type)];
+resolve_hosts(DomainName, srv) ->
+    Records = inet_res:lookup(DomainName, in, srv),
+    lists:usort(lists:map(fun({_, _, _, Host}) -> Host end, Records)).
+
+node_name(undefined, Host) ->
+    [Name | _] = string:tokens(atom_to_list(node()), "@"),
+    node_name(Name, Host);
+node_name(NodeName, Host) ->
+    list_to_atom(lists:concat([NodeName, "@", Host])).
+
+lock(_Options) ->
+    ignore.
+
+unlock(_Options) ->
+    ignore.
+
+register(_Options) ->
+    ignore.
+
+unregister(_Options) ->
+    ignore.
---
+++ src/discovery/classy_httpc.erl
@@ -0,0 +1,120 @@
+%%--------------------------------------------------------------------
+%% Copyright (c) 2019-2026 EMQ Technologies Co., Ltd. All Rights Reserved.
+%%
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%%
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%%
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+%%--------------------------------------------------------------------
+
+-module(classy_httpc).
+
+-export([ get/3
+        , get/4
+        , get/5
+        , post/3
+        , post/4
+        , put/3
+        , put/4
+        , delete/3
+        , delete/4
+        ]).
+
+-ifdef(TEST).
+-compile(export_all).
+-compile(nowarn_export_all).
+-endif.
+
+get(Addr, Path, Params) ->
+    get(Addr, Path, Params, []).
+
+get(Addr, Path, Params, Headers) ->
+    get(Addr, Path, Params, Headers, []).
+
+get(Addr, Path, Params, Headers, HttpOpts) ->
+    URL = build_url(Addr, Path, Params),
+    parse_response(hackney:request(get, URL, Headers, <<>>, HttpOpts)).
+
+post(Addr, Path, Params) ->
+    post(Addr, Path, Params, []).
+
+post(Addr, Path, Params, HttpOpts) ->
+    URL = build_url(Addr, Path),
+    Body = build_query(Params),
+    Headers = [{<<"Content-Type">>, <<"application/x-www-form-urlencoded">>}],
+    parse_response(hackney:request(post, URL, Headers, Body, HttpOpts)).
+
+put(Addr, Path, Params) ->
+    put(Addr, Path, Params, []).
+
+put(Addr, Path, Params, HttpOpts) ->
+    URL = build_url(Addr, Path),
+    Body = build_query(Params),
+    Headers = [{<<"Content-Type">>, <<"application/x-www-form-urlencoded">>}],
+    parse_response(hackney:request(put, URL, Headers, Body, HttpOpts)).
+
+delete(Addr, Path, Params) ->
+    delete(Addr, Path, Params, []).
+
+delete(Addr, Path, Params, HttpOpts) ->
+    URL = build_url(Addr, Path, Params),
+    parse_response(hackney:request(delete, URL, [], <<>>, HttpOpts)).
+
+-spec(build_url(string(), string()) -> binary()).
+build_url(Addr, Path) ->
+    iolist_to_binary([Addr, "/", Path]).
+
+build_url(Addr, Path, Params) ->
+    iolist_to_binary([build_url(Addr, Path), "?", build_query(Params)]).
+
+-if(?OTP_RELEASE >= 23).
+build_query(Params) when is_list(Params) ->
+    uri_string:compose_query([{safty(K), safty(V)} || {K, V} <- Params]).
+
+safty(A) when is_atom(A)    -> atom_to_list(A);
+safty(I) when is_integer(I) -> integer_to_list(I);
+safty(T) -> T.
+
+-else.
+build_query(Params) ->
+    string:join([urlencode(Param) || Param <- Params], "&").
+
+urlencode(L) when is_list(L) ->
+    http_uri:encode(L);
+urlencode({K, V}) ->
+    urlencode(K) ++ "=" ++ urlencode(V);
+urlencode(A) when is_atom(A) ->
+    urlencode(atom_to_list(A));
+urlencode(I) when is_integer(I) ->
+    urlencode(integer_to_list(I));
+urlencode(B) when is_binary(B) ->
+    urlencode(binary_to_list(B)).
+-endif.
+
+parse_response({ok, Status, _RespHeaders, Ref}) when Status =:= 200;
+                                                     Status =:= 201 ->
+    case hackney:body(Ref) of
+        {ok, Body} ->
+            %% case hackney_headers:parse(<<"content-type">>, RespHeaders) of
+            %%     {<<"application">>, <<"json">>, _} ->
+            %%         {ok, jsone:decode(Body)};
+            %%     CC ->
+            %%         {error, {unexpected_content_type, CC}}
+            %% end;
+            {ok, jsone:decode(Body)};
+        Err ->
+            {error, {get_body, Err}}
+    end;
+parse_response({ok, 204, _RespHeaders, _}) ->
+    {ok, []};
+parse_response({ok, Status, _RespHeaders, Body}) ->
+    {error, {Status, Body}};
+parse_response({error, Reason}) ->
+    {error, Reason}.
