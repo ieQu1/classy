2e7d86db59c64c3c913860c682d8c7f38521a8a6
ieQu1 <99872536+ieQu1@users.noreply.github.com>
2026-08-13 11:20:13 +0200

    test fixture: Add configurable timeout

--- src/test/classy_start_system_fixture.erl
+++ src/test/classy_start_system_fixture.erl
@@ -8,20 +8,28 @@
 %% behavior callbacks:
 -export([init_per_node/4, cleanup_per_node/5]).
 
--include_lib("familiar/include/familiar.hrl").
+-export_type([conf/0]).
+
+-type conf() :: #{timeout => timeout()}.
 
 %%================================================================================
 %% behavior callbacks
 %%================================================================================
 
--spec init_per_node(familiar:site(), node(), _, familiar_fixture:state()) -> {ok, familiar_fixture:state()}.
-init_per_node(Site, _Node, _, State) ->
-  ok = ?ON(Site, classy:start_system()),
+-spec init_per_node(familiar:site(), node(), conf(), familiar_fixture:state()) -> {ok, familiar_fixture:state()}.
+init_per_node(Site, _Node, Conf, State) ->
+  ok = familiar:call(Site, classy, start_system, [], timeout(Conf)),
   {ok, State}.
 
 -spec cleanup_per_node(familiar:site(), node(), _, familiar_fixture:state(), IsKill) -> ok | {error, _}
   when IsKill :: boolean().
-cleanup_per_node(Site, _, _, _, false) ->
-  ?ON(Site, classy:stop_system());
+cleanup_per_node(Site, _Node, Conf, _State, false) ->
+  ok = familiar:call(Site, classy, stop_system, [], timeout(Conf));
 cleanup_per_node(_, _, _, _, true) ->
   ok.
+
+-spec timeout(conf()) -> timeout().
+timeout(#{timeout := TO}) ->
+  TO;
+timeout(#{}) ->
+  5_000.
